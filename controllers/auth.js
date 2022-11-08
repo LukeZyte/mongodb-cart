e6e379c776d42926edeb27f061164a3dc40b004a
LukeZyte <lukasz.jarzab01@gmail.com>
2022-11-08 22:47:35 +0100

    add sessions

---
+++ controllers/auth.js
@@ -0,0 +1,31 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req.get("Cookie").split("=")[1] === "true";
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  // res.setHeader("Set-Cookie", "LoggedIn=true; HttpOnly");
+  User.findById("6367ec94085467d449494a3a")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.session.save((err) => {
+        console.log(err);
+        res.redirect("/");
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
